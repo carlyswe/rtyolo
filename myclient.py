da51735cb6b8bf02f6e0e9bf215bc801c0b3e91f
carlyswe <157024933+carlyswe@users.noreply.github.com>
2024-07-11 15:21:17 +0900

    Update myclient.py

--- myclient.py
+++ myclient.py
@@ -1,12 +1,14 @@
 from time import sleep
 import requests
-#import os
+import sys
 
 if __name__ == "__main__":
 
+    sleeptime = int(sys.argv[1])
+    
     for i in range(10000):
         #call our command
-        sleep(2)
+        sleep(sleeptime)
         #resp = requests.get('http://0.0.0.0:5000/predict?source=https://www.youtube.com/watch?v=MNn9qKG2UFI', verify=False)
         resp = requests.get('http://0.0.0.0:5000/predict?source=https://raw.githubusercontent.com/ultralytics/ultralytics/main/ultralytics/assets/zidane.jpg', verify=False)
 #we could provide it how long to sleep and how many iterations as command line args somewhere 
