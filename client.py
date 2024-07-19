c338325989ce97fcbb2b3623ff5a266625752da0
carlyswe <157024933+carlyswe@users.noreply.github.com>
2024-07-19 13:39:43 +0900

    Update client.py

--- client.py
+++ client.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         #call our command
         sleep(sleeptime)
         #resp = requests.get('http://0.0.0.0:5000/predict?source=https://www.youtube.com/watch?v=MNn9qKG2UFI', verify=False)
-        resp = requests.get('http://172.17.0.2:5000/predict?source=https://raw.githubusercontent.com/ultralytics/ultralytics/main/ultralytics/assets/zidane.jpg', verify=False)
+        resp = requests.get('http://117.16.143.43:5000/predict?source=https://raw.githubusercontent.com/ultralytics/ultralytics/main/ultralytics/assets/zidane.jpg', verify=False)
 #we could provide it how long to sleep and how many iterations as command line args somewhere 
