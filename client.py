78c9dc8551b59a370d09fa9d3ce7e28b5f69f90f
carlyswe <157024933+carlyswe@users.noreply.github.com>
2024-07-22 11:12:13 +0900

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
 
     sleeptime = float(sys.argv[1])
-    port = string(sys.argv[2])
+    port = str(sys.argv[2])
     url = 'http://117.16.143.43:' + port + 'predict?source=https://raw.githubusercontent.com/ultralytics/ultralytics/main/ultralytics/assets/zidane.jpg'
 
     print("url is ", url)
